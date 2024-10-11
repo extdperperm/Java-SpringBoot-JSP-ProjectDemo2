8d85c82c1af3f9f740854298f26f4ea50378245f
Daniel Pérez <118856521+extdperperm@users.noreply.github.com>
2024-10-11 17:14:37 +0100

    Update MainController.java

--- src/main/java/es/dsw/controllers/MainController.java
+++ src/main/java/es/dsw/controllers/MainController.java
@@ -1,8 +1,8 @@
 package es.dsw.controllers;
  
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.Cookie;
  
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
